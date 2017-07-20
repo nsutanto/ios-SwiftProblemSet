0a03f0939cf337e2171d450b833cc7fb5f2b3163
nsutanto <nicholas.sutanto@cummins.com>
2017-07-19 21:07:24 -0400

    Update for answers (#2)
    * Finish If Statements
    
    Finish If Statements
    
    * Complete Exercise 14-17

--- SwiftProblemSet.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ SwiftProblemSet.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -5,27 +5,27 @@
 //: ## Functions 
 //: ### Exercise 14
 //: The function `emojiLove` should take two `String` parameters and use them to print a `String` with  the format "stringParameterOne ❤️ stringParameterTwo".
-func emojiLove(/* add parameters here */) {
-    /* add function implementation here */
+func emojiLove(s1: String, s2: String) {
+    print(s1 + " \u{1F496} " + s2)
 }
 
-/* Example Function Call
- 
+
  emojiLove(s1: "cats", s2: "dogs") // prints "cats ❤️ dogs"
  emojiLove(s1: "udacity", s2: "students") // prints "udacity ❤️ students"
  emojiLove(s1: "peanut butter", s2: "jelly") // prints "peanut butter ❤️ jelly"
  emojiLove(s1: "ying", s2: "yang") // prints "ying ❤️ yang"
  
- */
 
 //: ### Exercise 15
 //: The function `median` should take three `Int` parameters and return the `Int` value in the middle.
-func median(/* add parameters here */) /* define the return type */ {
-    /* add function implementation here */
+func median(num1: Int, num2: Int, num3: Int) -> Int{
+    var array = [num1, num2, num3]
+    array.sort(by: <)
+    print(array[1])
+    return array[1]
 }
 
-/* Example Function Call
- 
+
  median(num1: 1, num2: 5, num3: 6) // 5
  median(num1: 2, num2: 1, num3: 4) // 2
  median(num1: 3, num2: 6, num3: 6) // 6
@@ -34,7 +34,6 @@ func median(/* add parameters here */) /* define the return type */ {
  median(num1: 2, num2: 3, num3: 1) // 2
  median(num1: 2, num2: 2, num3: 1) // 2
  
- */
 
 /*:
  ### Exercise 16
@@ -46,19 +45,36 @@ func median(/* add parameters here */) /* define the return type */ {
  
  **Note**: It is assumed that the input string is given in English.
  */
-func beginsWithVowel(/* add parameters here */) /* define the return type */ {
+func beginsWithVowel(inputString: String) -> Bool {
     /* add function implementation here */
+    var retVal: Bool = false
+    if (inputString == "") {
+        retVal = false
+    }
+    else {
+        if (inputString.characters[inputString.startIndex] == "A" ||
+            inputString.characters[inputString.startIndex] == "a" ||
+            inputString.characters[inputString.startIndex] == "E" ||
+            inputString.characters[inputString.startIndex] == "e" ||
+            inputString.characters[inputString.startIndex] == "I" ||
+            inputString.characters[inputString.startIndex] == "i" ||
+            inputString.characters[inputString.startIndex] == "O" ||
+            inputString.characters[inputString.startIndex] == "o" ||
+            inputString.characters[inputString.startIndex] == "U" ||
+            inputString.characters[inputString.startIndex] == "u" ) {
+            retVal = true;
+        }
+    }
+    
+    return retVal
 }
 
-/* Example Function Call
- 
- beginsWithVowel("Apples") // true
- beginsWithVowel("pIG") // false
- beginsWithVowel("oink") // true
- beginsWithVowel("udacity") // true
- beginsWithVowel("") // false
- 
- */
+
+beginsWithVowel(inputString: "Apples") // true
+beginsWithVowel(inputString: "pIG") // false
+beginsWithVowel(inputString: "oink") // true
+beginsWithVowel(inputString: "udacity") // true
+beginsWithVowel(inputString: "") // false
 
 /*:
  ### Exercise 17
@@ -71,19 +87,26 @@ func beginsWithVowel(/* add parameters here */) /* define the return type */ {
  
  **Hint**: Re-use the `beginsWithVowel` function.
  */
-func funWithWords(/* add parameters here */) /* define the return type */ {
-    /* add function implementation here */
+func funWithWords(str: String) -> String {
+    var retValue: String = ""
+    if (beginsWithVowel(inputString: str)) {
+        retValue = str.uppercased()
+    }
+    else {
+        retValue = str.lowercased()
+    }
+    
+    return retValue
 }
 
-/* Example Function Call
+
+funWithWords(str: "Apples") // "APPLES"
+funWithWords(str: "pIG") // "pig"
+funWithWords(str: "oink") // "OINK"
+funWithWords(str: "udacity") // "UDACITY"
+funWithWords(str: "") // ""
  
- funWithWords("Apples") // "APPLES"
- funWithWords("pIG") // "pig"
- funWithWords("oink") // "OINK"
- funWithWords("udacity") // "UDACITY"
- funWithWords("") // ""
- 
- */
+
 /*:
  ****
  [Table of Contents](Table%20of%20Contents) | [Previous](@previous) | [Next](@next)
